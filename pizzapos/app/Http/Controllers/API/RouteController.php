21e3a666858787920aac7d70acb2ff8eaf460991
Sitnyein <chankhinem@gmail.com>
2023-12-26 15:35:01 +0630

    api piza crud

--- pizzapos/app/Http/Controllers/API/RouteController.php
+++ pizzapos/app/Http/Controllers/API/RouteController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\API;
 
+use Carbon\Carbon;
 use App\Models\User;
 use App\Models\Order;
+use App\Models\Contact;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -45,6 +47,55 @@ class RouteController extends Controller
         return response()->json($data, 200);
 }
 
+//create api contact
+    public function apicontact(Request $req) {
+       $data = [
+        'name' => $req->name,
+        'email' => $req->email,
+        'message' => $req->message,
+        'created_at' =>Carbon::now(),
+        'updated_at' =>Carbon::now()
+
+       ];
+       Contact::create($data);
+
+       $person = Contact::orderBy('id','desc')->get();
+
+       return response()->json($person, 200);
+    }
+
+    public function delcontact(request $req) {
+       $data = Contact::where('id',$req->contact_id)->first();
+        //    dd(isset($data));
+       if(isset($data)) {
+         return response()->json(['message' => 'delete success','data' => $data], 200);
+
+
+       }
+    return response()->json(['message' => 'This id no found any data'], 200);
+
+    }
+
+    public function updatecontact(Request $req) {
+        $olddata = Contact::where('id',$req->id)->first();
+       if(isset($olddata)) {
+        $data = [
+            'name' => $req->name,
+            'email' => $req->email,
+            'message' => $req->message,
+            'updated_at' =>Carbon::now()
+
+           ];
+        Contact::where('id',$req->id)->update($data);
+        $newdata =  Contact::where('id',$req->id)->first();
+
+          return response()->json(['newdata' => $newdata,'olddata' => $olddata], 200);
+       }
+
+         return response()->json(['message' => 'you can not update data'], 200, );
+        // return $data;
+        // dd($req->all());
+    }
 
 
 
@@ -52,3 +103,11 @@ class RouteController extends Controller
 
 
 }
+// $data = [
+//     'name' => $req->name,
+//     'email' => $req->email,
+//     'message' => $req->message,
+//     'updated_at' =>Carbon::now()
+
+//    ];
+// return response()->json([$olddata], 200);
--- pizzapos/routes/api.php
+++ pizzapos/routes/api.php
@@ -23,6 +23,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('user/order',[RouteController::class,'apiuser']);
 Route::get('category/product',[RouteController::class,'apiproduct']);
 Route::get('user/product',[RouteController::class,'userproduct']);
+//create api contact
+Route::post('contact',[RouteController::class,'apicontact']);
+
+//delete api contact
+Route::post('contact/del',[RouteController::class,'delcontact']);
+
+//update api contact
+Route::post('contact/update',[RouteController::class,'updatecontact']);
 
 Route::get('apitesting',function() {
     $data = [
